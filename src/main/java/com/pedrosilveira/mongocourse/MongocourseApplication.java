5a829b517813da276f5ddfb8b636c6029369aa6a
PedroRSilveira <silveirapedro2003@gmail.com>
2023-07-13 15:18:26 -0300

    Primeiro commit - Projeto criado

---
+++ src/main/java/com/pedrosilveira/mongocourse/MongocourseApplication.java
@@ -0,0 +1,13 @@
+package com.pedrosilveira.mongocourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongocourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongocourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedrosilveira/mongocourse/MongocourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrosilveira.mongocourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongocourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
