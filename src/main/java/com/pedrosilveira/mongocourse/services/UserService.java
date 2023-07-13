8186514f4a9c1e78afebc8ea40c18da4c4721228
PedroRSilveira <silveirapedro2003@gmail.com>
2023-07-13 19:17:39 -0300

    Obtendo um usuario por id

--- src/main/java/com/pedrosilveira/mongocourse/services/UserService.java
+++ src/main/java/com/pedrosilveira/mongocourse/services/UserService.java
@@ -1,12 +1,14 @@
 package com.pedrosilveira.mongocourse.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.pedrosilveira.mongocourse.domain.User;
 import com.pedrosilveira.mongocourse.repository.UserRepository;
+import com.pedrosilveira.mongocourse.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
